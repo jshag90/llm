6d88941d3a110a128395368249c198dc4b99cf00
seonhakji <jshag1990@gmail.com>
2025-04-22 14:09:22 +0900

    [Feat] 데이터보호, 시스템 정책 스키마만 전달

--- src/main/java/com/smt/ai/llm/controller/LlmController.java
+++ src/main/java/com/smt/ai/llm/controller/LlmController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
--- src/main/java/com/smt/ai/llm/util/PromptTemplateUtil.java
+++ src/main/java/com/smt/ai/llm/util/PromptTemplateUtil.java
@@ -16,24 +16,24 @@ public class PromptTemplateUtil {
     }
 
     public static String getSchemaPrompt(JdbcTemplate jdbcTemplate, String condition){
-        String tableSchemas = getTableSchema(jdbcTemplate, "ds_file_protect_log") + "(데이터 보호/차단 로그 테이블)";
-        tableSchemas += getTableSchema(jdbcTemplate, "ds_policy_admin_sys") + "(시스템 정책 테이블)";
-        tableSchemas += getTableSchema(jdbcTemplate, "ds_policy_data_protect_log") + "(데이터 정책 로그 테이블)";
-        tableSchemas += getTableSchema(jdbcTemplate, "ds_hr_group") + "(인사연동테이블)";
-        tableSchemas += getTableSchema(jdbcTemplate, "ds_client") + "(에이전트/클라이언트 테이블)";
+        String tableSchemas = getTableSchema(jdbcTemplate, "ds_file_protect_log", "데이터 보호/차단 로그 테이블");
+        tableSchemas += getTableSchema(jdbcTemplate, "ds_policy_admin_sys", "시스템 정책 테이블") ;
+     /*   tableSchemas += getTableSchema(jdbcTemplate, "ds_policy_data_protect_log", "데이터 정책 로그 테이블");
+        tableSchemas += getTableSchema(jdbcTemplate, "ds_hr_group", "인사연동테이블");
+        tableSchemas += getTableSchema(jdbcTemplate, "ds_client", "에이전트/클라이언트 테이블");*/
 
-        return "You are a SQL expert. "
+        return "You are a SQL expert. Given the schema below, "
                 + tableSchemas +
                 "\nGenerate a SQL query for this request: \"" + condition + "\""
-                + ".Return only the SQL query.";
+                + ".Output only the SQL query without any explanation.";
     }
 
-    private static String getTableSchema(JdbcTemplate jdbcTemplate, String tableName) {
+    private static String getTableSchema(JdbcTemplate jdbcTemplate, String tableName, String tableInfo) {
         String query = "SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_COMMENT " +
                 "FROM INFORMATION_SCHEMA.COLUMNS " +
                 "WHERE TABLE_NAME = ?";
 
-        StringBuilder schemaBuilder = new StringBuilder("Here is the schema of the \"" + tableName + "\" table:\n");
+        StringBuilder schemaBuilder = new StringBuilder("Here is the schema of the \"" + tableName+"("+tableInfo +")"+ "\" table:\n");
         jdbcTemplate.query(query, new Object[]{tableName}, (rs) -> {
             schemaBuilder.append("- ")
                     .append(rs.getString("COLUMN_NAME"))
